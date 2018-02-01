99453b5eb10dc602657f20f8f1653a538dbefc80
DB <barkoczi>
2018-02-01 12:39:35 +0100

    .

--- instructions.R
+++ instructions.R
@@ -2,9 +2,11 @@
 
 #step 2: download the repository:
 
-#git clone git@github.com:dnlbrkc/tardis.git"
+'git clone git@github.com:dnlbrkc/tardis.git'
 
-#install packages: install.packages("rprojroot")
+#install packages:
+
+install.packages("rprojroot")
 
 #step 3: generate jester dataset
 
@@ -12,16 +14,14 @@ library(rprojroot)
 path <- function(x) rprojroot::find_root_file(x, criterion = has_file("social-recommendation.Rproj"))
 setwd(path("/jesterDatasets/"))
 source(path("/jesterDatasets/get_data.R"))
-
-generate_data() #this will download and save the jester dataset using a random seed
+generate_data()
 
 #step 4:
-
 setwd(path("simulationCode"))
 
 #step 5: run simulation job for 1000 repetitions
 
-#echo ‘export OMP_NUM_THREADS=1; Rscript jesterBaseline.R $PBS_ARRAYID’ | qsub -d. -t 1-1000 -N baseline
+'echo ‘export OMP_NUM_THREADS=1; Rscript jesterBaseline.R $PBS_ARRAYID’ | qsub -d. -t 1-1000 -N baseline'
 
 #step 6: once the job is ready, aggregate individual simulation runs using the following code:
 
@@ -34,7 +34,7 @@ for(f in files){
 }
 baseline <- baseline / length(files)
 
-save(baseline,file="baseline.Rdata")
+save(baseline,file="resultsBaseline.Rdata")
 
 
 #step 7: push results to the repo
